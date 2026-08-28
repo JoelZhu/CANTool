0f082e10380fe93a2cd1adfe5eefc3d5787026da
zhuqian <zhuqian@aiv2x.com>
2026-08-28 17:12:42 +0800

    修改window打包脚本

--- packageWindows.bat
+++ packageWindows.bat
@@ -16,9 +16,9 @@ if exist "%folder_to_delete%" (
 
 echo.
 echo 正在打包...
-".venv\Scripts\pyinstaller.exe" --add-data "ui/material_dark_style.qss;ui" \
-                                --add-data "ui/material_light_style.qss;ui" \
-                                --add-data "app_icon.ico;." \
+".venv\Scripts\pyinstaller.exe" --add-data "ui/material_dark_style.qss;ui" ^
+                                --add-data "ui/material_light_style.qss;ui" ^
+                                --add-data "app_icon.ico;." ^
                                 Main.py --windowed
 
 echo.
